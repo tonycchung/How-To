7dce62411af92e3d96aa4322254ff0b427ab5c1b
Tony Chung <tonycchung@users.noreply.github.com>
2014-05-30 12:17:28 -0700

    Update steps_controller.rb
    Remove :list_id from required parameters.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -77,6 +77,6 @@ class StepsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def step_params
-      params.require(:step).permit(:body, :list_id)
+      params.require(:step).permit(:body)
     end
 end
